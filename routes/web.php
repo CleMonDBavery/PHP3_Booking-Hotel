c137f409e9b2a7072a246d21118903f14998a4e1
KIEN <kienap12f@gmail.com>
2024-07-26 17:19:26 +0700

    N05 - project

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
 
 // Route::get('/', [AdminCategoryController::class, 'index']);
 // Route::get('/san-pham', [AdminCategoryController::class, 'index']);
@@ -85,3 +85,16 @@ Route::prefix('admin')->group(function () {
         return view('admin.insert_service');
     })->name('insert_service');
 });
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
